af92a56be24791d800a83687b5cbc13e936a02c6
jmercado1 <mercadojesus1111@gmail.com>
2023-08-27 00:37:15 -0500

    first commit

---
+++ console.js
@@ -0,0 +1,10 @@
+console.log("Jesus Mercado")
+console.log("2002")
+console.log(2002)
+console.log("Jesus Mercado (2002)");
+console.log("Jesus", " Mercado ", "(2002)");
+console.log("Jesus");
+console.log("Mercado ");
+console.log("(2002)");
+console.log("Jesus" + " Mercado" + "(2002)");
+console.log("Jesus " + " Mercado " + "(2002)");
